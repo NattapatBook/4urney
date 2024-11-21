f6d47940c746779a646ba970eaa76f1e01651225
Thanapath Lee <thanapath@4amconsult.com>
2024-11-22 01:14:22 +0700

    chat_center : add path list_dashboard

--- apps/chat_center/urls.py
+++ apps/chat_center/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 # from apps.chat_center.views import my_api, CustomerViewSet
-from apps.chat_center.views import list_user, list_message, admin_reply_post, change_message_type, list_user_test
+from apps.chat_center.views import list_user, list_message, admin_reply_post, change_message_type, list_user_test, list_dashboard
 
 # router = DefaultRouter()
 # router.register('customer', CustomerViewSet)
@@ -14,5 +14,6 @@ urlpatterns = [
     path('list_message/<user_id>', list_message),
     path('admin_reply_post/', admin_reply_post),
     path('change_message_type/', change_message_type),
+    path('list_dashboard/', list_dashboard)
     # *router.urls,
 ]
