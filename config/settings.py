61bfb40efbf900e5bb14421e0ebc47f9910bb809
Thanapath Lee <thanapath@4amconsult.com>
2024-12-20 15:18:51 +0700

    uncomment settings.py

--- config/settings.py
+++ config/settings.py
@@ -77,26 +77,26 @@ AWS_S3_REGION_NAME = ENV('AWS_S3_REGION_NAME', str, 'ap-southeast-1')
 
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
-# STORAGES = {
-#     'default': {
-#         'BACKEND': 'storages.backends.s3boto3.S3Boto3Storage',
-#         'OPTIONS': {
-#             'access_key': AWS_ACCESS_KEY_ID,
-#             'secret_key': AWS_SECRET_ACCESS_KEY,
-#             'bucket_name': AWS_STORAGE_BUCKET_NAME,
-#             'region_name': AWS_S3_REGION_NAME,
-#         },
-#     },
-#     'staticfiles': {
-#         'BACKEND': 'storages.backends.s3boto3.S3Boto3Storage',
-#         'OPTIONS': {
-#             'access_key': AWS_ACCESS_KEY_ID,
-#             'secret_key': AWS_SECRET_ACCESS_KEY,
-#             'bucket_name': AWS_STORAGE_BUCKET_NAME,
-#             'region_name': AWS_S3_REGION_NAME,
-#         },
-#     },
-# }
+STORAGES = {
+    'default': {
+        'BACKEND': 'storages.backends.s3boto3.S3Boto3Storage',
+        'OPTIONS': {
+            'access_key': AWS_ACCESS_KEY_ID,
+            'secret_key': AWS_SECRET_ACCESS_KEY,
+            'bucket_name': AWS_STORAGE_BUCKET_NAME,
+            'region_name': AWS_S3_REGION_NAME,
+        },
+    },
+    'staticfiles': {
+        'BACKEND': 'storages.backends.s3boto3.S3Boto3Storage',
+        'OPTIONS': {
+            'access_key': AWS_ACCESS_KEY_ID,
+            'secret_key': AWS_SECRET_ACCESS_KEY,
+            'bucket_name': AWS_STORAGE_BUCKET_NAME,
+            'region_name': AWS_S3_REGION_NAME,
+        },
+    },
+}
 
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
