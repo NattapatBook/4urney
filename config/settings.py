d6ff085682fac04aaff15e8fc2205b6503b14524
Thanapath Lee <thanapath@4amconsult.com>
2024-12-20 14:58:19 +0700

    update settings.py test build

--- config/settings.py
+++ config/settings.py
@@ -68,10 +68,11 @@ INSTALLED_APPS = [
 ]
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_ACCESS_KEY_ID = ENV('AWS_ACCESS_KEY_ID', str, '')
 AWS_SECRET_ACCESS_KEY = ENV('AWS_SECRET_ACCESS_KEY', str, '')
-AWS_STORAGE_BUCKET_NAME = ENV('AWS_STORAGE_BUCKET_NAME', str, '')
+AWS_STORAGE_BUCKET_NAME = ENV('AWS_STORAGE_BUCKET_NAME', str, '4urney-dev-data-model')
 AWS_S3_REGION_NAME = ENV('AWS_S3_REGION_NAME', str, 'ap-southeast-1')
 
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
@@ -96,13 +97,6 @@ STORAGES = {
         },
     },
 }
-
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-# AWS_S3_OBJECT_PARAMETERS = {
-#     'CacheControl': 'max-age=86400',
-# }
-# AWS_DEFAULT_ACL = 'public-read'
 
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
