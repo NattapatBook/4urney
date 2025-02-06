f61fb96bb79b41bc2c27803aa6e6a8e3611137d9
Thanapath Lee <thanapath@4amconsult.com>
2025-02-06 15:30:49 +0700

    update admin

--- apps/webhook_line/admin.py
+++ apps/webhook_line/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from apps.webhook_line.models import LineIntegration, LineConnection
+from apps.webhook_line.models import LineIntegration, LineConnection, LineConnectionNew
 
 # Register your models here.
 
 admin.site.register(LineIntegration)
 admin.site.register(LineConnection)
+admin.site.register(LineConnectionNew)
